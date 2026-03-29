01c9b72b3414b749278f738356ab24a21a54eb42
sta5048 <esder5048@gmail.com>
2026-03-30 08:38:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 def load_duty(selected_date):
-    filename = f"data/duty_{selected_date.year}_{selected_date.month:02d}.csv"
+    filename = f"data/duty_{selected_date.year}_{selected_date.month:02d}.csv
     if not os.path.exists(filename): return None
     with open(filename, "r", encoding="utf-8") as f:
         return [line.strip().split(",") for line in f]
